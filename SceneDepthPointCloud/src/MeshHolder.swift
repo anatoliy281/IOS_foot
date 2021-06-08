c4bdb78403c7e2625ac10e606bf3e6f13a9e574b
anatoliy281 <chernov281@gmail.com>
2021-06-08 10:43:49 +0300

    tmp  Отключили  MeshHolder и с ней запись в obj.

--- SceneDepthPointCloud/src/MeshHolder.swift
+++ SceneDepthPointCloud/src/MeshHolder.swift
@@ -40,31 +40,31 @@ class MeshHolder {
 	}
 	
 		
-	func convertToObj() -> GroupedData {
-		let res = GroupedData()
-		// не выводим сетки по типу узла
-//		for key in coords.data.keys {
-//			res.data[key] = writeEdges(input: key)[0]
+//	func convertToObj() -> GroupedData {
+//		let res = GroupedData()
+//		// не выводим сетки по типу узла
+////		for key in coords.data.keys {
+////			res.data[key] = writeEdges(input: key)[0]
+////		}
+//		
+//		// а выводим только сетки ноги и их контуры
+//		let meshesAndContours = writeEdges( input: Int(Foot.rawValue), toSmooth: true, toTruncFloor: true )
+//		// вывод контуров сперва исходный, затем сглаженный -> обрезанный -> контуры
+//		for i in 0..<meshesAndContours.count {
+//			let contourId = Int(Foot.rawValue) + i
+//			res.data[contourId] = meshesAndContours[i]	// исходная сетка i = 0 (id = 2), сглаженная i = 1 (id = 3), обрезанная i = 2 (id = 4), слои i = 3,4,... (id = 5, 6, ...)
+////			if 1 <= i && i <= 2 { // i = 1, 2 (id = 3, 4)
+////				debugMeshesId.append(contourId)
+////			} else {
+////				if contourId != cutContourId { // контуры начинаются отсчитываться с 3 позиции i = 3 (номер контура или слоя 0)
+////					contoursId.append(contourId)
+////				}
+////
+////			}
 //		}
-		
-		// а выводим только сетки ноги и их контуры
-		let meshesAndContours = writeEdges( input: Int(Foot.rawValue), toSmooth: true, toTruncFloor: true )
-		// вывод контуров сперва исходный, затем сглаженный -> обрезанный -> контуры
-		for i in 0..<meshesAndContours.count {
-			let contourId = Int(Foot.rawValue) + i
-			res.data[contourId] = meshesAndContours[i]	// исходная сетка i = 0 (id = 2), сглаженная i = 1 (id = 3), обрезанная i = 2 (id = 4), слои i = 3,4,... (id = 5, 6, ...)
-//			if 1 <= i && i <= 2 { // i = 1, 2 (id = 3, 4)
-//				debugMeshesId.append(contourId)
-//			} else {
-//				if contourId != cutContourId { // контуры начинаются отсчитываться с 3 позиции i = 3 (номер контура или слоя 0)
-//					contoursId.append(contourId)
-//				}
-//
-//			}
-		}
-		
-		return res
-	}
+//		
+//		return res
+//	}
 	
 	// сгруппировать данные по группам (группа точек/номер кадра)
 	private func separateData() -> GroupDataCoords {
@@ -105,36 +105,36 @@ class MeshHolder {
 	}
 
 	// упаковать данные в строку
-	private func writeEdges(input key: Int = Int(Foot.rawValue),
-							toSmooth: Bool = false,
-							toTruncFloor: Bool = false) -> [String] {
-		fullTable(Int(Foot.rawValue))
-		var resArr:[String] = [writeNodes()]
-		
-		if toSmooth {
-			smooth()
-			resArr.append(writeNodes())
-		}
-
-		if toTruncFloor {
-			let meshAndContoursCoords = truncateTheFloor()
-			resArr.append(writeNodes(meshAndContoursCoords.last!, connectNodes: false)) // выводим обрезанную сетку
-			for i in 0..<meshAndContoursCoords.count-1 {		// выводим контуры
-				resArr.append(writeNodes(meshAndContoursCoords[i]))
-			}
-			
-		}
-		
+//	private func writeEdges(input key: Int = Int(Foot.rawValue),
+//							toSmooth: Bool = false,
+//							toTruncFloor: Bool = false) -> [String] {
+//		fullTable(Int(Foot.rawValue))
+//		var resArr:[String] = [writeNodes()]
+//
+//		if toSmooth {
+//			smooth()
+//			resArr.append(writeNodes())
+//		}
+//
+//		if toTruncFloor {
+//			let meshAndContoursCoords = truncateTheFloor()
+//			resArr.append(writeNodes(meshAndContoursCoords.last!, connectNodes: false)) // выводим обрезанную сетку
+//			for i in 0..<meshAndContoursCoords.count-1 {		// выводим контуры
+//				resArr.append(writeNodes(meshAndContoursCoords[i]))
+//			}
+//
+//		}
+//
+//
+//		return resArr
+//	}
 	
-		return resArr
-	}
-	
-	private	func fullTable( _ key: Int ) {
-		table = Array(repeating:Array(repeating: Float(), count: dim), count: dim)
-		for ( i, j, val ) in coords.data[key]! {
-			table[i][j] = val
-		}
-	}
+//	private	func fullTable( _ key: Int ) {
+//		table = Array(repeating:Array(repeating: Float(), count: dim), count: dim)
+//		for ( i, j, val ) in coords.data[key]! {
+//			table[i][j] = val
+//		}
+//	}
 	   
 	   
    private func calcCoords(_ i:Int, _  j:Int, _ table: inout [[Float]]) -> Float3 {
@@ -157,21 +157,21 @@ class MeshHolder {
 	   
 	   
 	   
-	private func smooth() {
-		for theta in 1..<dim-1 {
-			for phi in 0..<dim {
-				let phi_prev = (phi-1 + dim)%dim
-				let phi_next = (phi+1 + dim)%dim
-				var mask:[Float] = [
-						table[theta-1][phi_prev], table[theta-1][phi], table[theta-1][phi_next],
-						table[theta][phi_prev],   table[theta][phi],   table[theta][phi_next],
-						table[theta+1][phi_prev], table[theta+1][phi], table[theta+1][phi_next]
-									]
-				mask.sort()
-				table[theta][phi] = mask[4]
-			}
-	   }
-   }
+//	private func smooth() {
+//		for theta in 1..<dim-1 {
+//			for phi in 0..<dim {
+//				let phi_prev = (phi-1 + dim)%dim
+//				let phi_next = (phi+1 + dim)%dim
+//				var mask:[Float] = [
+//						table[theta-1][phi_prev], table[theta-1][phi], table[theta-1][phi_next],
+//						table[theta][phi_prev],   table[theta][phi],   table[theta][phi_next],
+//						table[theta+1][phi_prev], table[theta+1][phi], table[theta+1][phi_next]
+//									]
+//				mask.sort()
+//				table[theta][phi] = mask[4]
+//			}
+//	   }
+//   }
 	   
 //	private func truncateTheFloor(table: inout [[Float]]) {
 ////		let k0 = Float(3)
@@ -211,78 +211,78 @@ class MeshHolder {
 //	}
 	
 	// вначале записывает контуры, затем в конце итоговую сетку
-	private func truncateTheFloor() -> [[Float3]] {
-		
-		var res:[[Float3]] = []		// содержит и контуры и обрезанную сетку
-		var perimeters:[(height:Float, value:Float)] = []
-		let d:Float = 1
-		let nLayers = 20
-		for n in 0..<nLayers {
-			let layerHeight = d*Float(n)
-			if let loop = calcCurveLoop(layerHeight) {
-				let perimeter = calcPerimeter(loop)
-				if perimeter > 0 {
-					perimeters.append((height: layerHeight, value: perimeter))
-					res.append(loop)
-				}
-				
-			}
-		}
-		
-		layersNumber = res.count
-		
-		
-		(thisLayer, cutHeight) = checkPerimeter(perimeters)
-		
-
-		var mesh:[Float3] = []
-		for i in 0..<dim {	// theta
-			for j in 0..<dim { // phi
-				let pos = calcCoords(i, j, &table)
-				mesh.append(pos)
-			}
-		}
-		mesh.removeAll(where: {$0.z < cutHeight})
-		res.append(mesh)
-		return res
-	}
+//	private func truncateTheFloor() -> [[Float3]] {
+//
+//		var res:[[Float3]] = []		// содержит и контуры и обрезанную сетку
+//		var perimeters:[(height:Float, value:Float)] = []
+//		let d:Float = 1
+//		let nLayers = 20
+//		for n in 0..<nLayers {
+//			let layerHeight = d*Float(n)
+//			if let loop = calcCurveLoop(layerHeight) {
+//				let perimeter = calcPerimeter(loop)
+//				if perimeter > 0 {
+//					perimeters.append((height: layerHeight, value: perimeter))
+//					res.append(loop)
+//				}
+//
+//			}
+//		}
+//
+//		layersNumber = res.count
+//
+//
+//		(thisLayer, cutHeight) = checkPerimeter(perimeters)
+//
+//
+//		var mesh:[Float3] = []
+//		for i in 0..<dim {	// theta
+//			for j in 0..<dim { // phi
+//				let pos = calcCoords(i, j, &table)
+//				mesh.append(pos)
+//			}
+//		}
+//		mesh.removeAll(where: {$0.z < cutHeight})
+//		res.append(mesh)
+//		return res
+//	}
 	
-	private func calcCurveLoop(_ cutHeight:Float ) -> [Float3]? {
-		var points: [Float3] = []	// without z coords only x and y
-		for nPhi in 0..<dim {	// theta
-			var dMin:Float = 5
-			var closestPoint:Float3?
-			for nTheta in 0..<dim { // phi
-				let pos = calcCoords(nTheta, nPhi, &table)
-				if dot(pos, pos) > Float(0) {
-					let dH = abs(pos.z - cutHeight)
-					if dH < dMin {
-						dMin = dH
-						closestPoint = pos
-					}
-				}
-			}
-			if let p = closestPoint {
-				points.append(p)
-			}
-		}
-		if !points.isEmpty {
-			points.append(points[0]) // close in loop
-			return points
-		} else {
-			return nil
-		}
-	}
-	
-	private func calcPerimeter(_ points:[Float3]) -> Float {
-		var res:Float = 0
-
-		for i in 0..<points.count-1 { // calc perimeter
-			let dr = points[i] - points[i+1]
-			res += sqrt(dot(dr, dr))
-		}
-		return res
-	}
+//	private func calcCurveLoop(_ cutHeight:Float ) -> [Float3]? {
+//		var points: [Float3] = []	// without z coords only x and y
+//		for nPhi in 0..<dim {	// theta
+//			var dMin:Float = 5
+//			var closestPoint:Float3?
+//			for nTheta in 0..<dim { // phi
+//				let pos = calcCoords(nTheta, nPhi, &table)
+//				if dot(pos, pos) > Float(0) {
+//					let dH = abs(pos.z - cutHeight)
+//					if dH < dMin {
+//						dMin = dH
+//						closestPoint = pos
+//					}
+//				}
+//			}
+//			if let p = closestPoint {
+//				points.append(p)
+//			}
+//		}
+//		if !points.isEmpty {
+//			points.append(points[0]) // close in loop
+//			return points
+//		} else {
+//			return nil
+//		}
+//	}
+//
+//	private func calcPerimeter(_ points:[Float3]) -> Float {
+//		var res:Float = 0
+//
+//		for i in 0..<points.count-1 { // calc perimeter
+//			let dr = points[i] - points[i+1]
+//			res += sqrt(dot(dr, dr))
+//		}
+//		return res
+//	}
 	
 	private func checkPerimeter ( _ perimeters: [(height:Float, value:Float)] ) -> (layer:Int, height:Float) {
 
@@ -318,42 +318,42 @@ class MeshHolder {
 
 	}
 	
-	private func writeNodes() -> String {
-		var res:String = ""
-		for i in 0..<dim {
-			for j in 0..<dim {
-				var str = ""
-				if table[i][j] != Float() {
-					let pos = calcCoords(i, j, &table)
-					str = "v \(pos.x) \(pos.y) \(pos.z)\n"
-				} else {
-					if (renderer.currentState != .separate) {
-						str = "v 0 0 0\n"
-					}
-				}
-				res.append(str)
-			}
-		}
-		
-		if (renderer.currentState != .separate) {
-			for i in 0..<dim {
-				for j in 0..<dim {
-					if (table[i][j] != Float()) {
-						if (j+1 != dim && table[i][j+1] != Float()) {
-							let index = i*dim + j
-							res.append("l \(index+1) \(index+2)\n")
-						}
-						if (i+1 != dim && table[i+1][j] != Float()) {
-							let index = (i+1)*dim + j
-							res.append("l \(index-dim+1) \(index+1)\n")
-						}
-					}
-				}
-			}
-		}
-		
-		return res
-	}
+//	private func writeNodes() -> String {
+//		var res:String = ""
+//		for i in 0..<dim {
+//			for j in 0..<dim {
+//				var str = ""
+//				if table[i][j] != Float() {
+//					let pos = calcCoords(i, j, &table)
+//					str = "v \(pos.x) \(pos.y) \(pos.z)\n"
+//				} else {
+//					if (renderer.currentState != .separate) {
+//						str = "v 0 0 0\n"
+//					}
+//				}
+//				res.append(str)
+//			}
+//		}
+//
+//		if (renderer.currentState != .separate) {
+//			for i in 0..<dim {
+//				for j in 0..<dim {
+//					if (table[i][j] != Float()) {
+//						if (j+1 != dim && table[i][j+1] != Float()) {
+//							let index = i*dim + j
+//							res.append("l \(index+1) \(index+2)\n")
+//						}
+//						if (i+1 != dim && table[i+1][j] != Float()) {
+//							let index = (i+1)*dim + j
+//							res.append("l \(index-dim+1) \(index+1)\n")
+//						}
+//					}
+//				}
+//			}
+//		}
+//
+//		return res
+//	}
 	
 	private func writeNodes(_ coords: [Float3], connectNodes:Bool = true) -> String {
 		var res:String = ""
